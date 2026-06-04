bb3bde3d480feba13757098ae00d9e1e304c8a99
vishal <vishalbabar7351@gmail.com>
2026-06-04 13:30:53 +0530

    first commit

---
+++ Lecture_num_01/index.js
@@ -0,0 +1,18 @@
+console.log('Vishal Babar')
+
+let a = 22; 
+let name = 'vishal babar'
+console.log('What is use age', a)
+console.log(name)   
+
+const data = () => {
+    console.log('Vishal Babar')
+
+let a = 22; 
+let name = 'vishal babar'
+console.log('What is use age', a)
+console.log(name)
+
+}
+
+data()
