fcc3a620bfe77e8dfcbab6051f64c6a9167163d9
vishal <vishalbabar7351@gmail.com>
2026-06-04 13:57:10 +0530

    var, let, const

---
+++ Lecture_num_04/index.js
@@ -0,0 +1,15 @@
+// Let, const and var.....................
+
+const account_Id = 2222222; 
+
+let account_Email = "vishalbabar7351@gmail.com";
+account_Email = "vishal3333@gmail.com";
+console.log(account_Email);
+
+var account_password = "12356"; 
+account_password = "7890000"; 
+console.log(account_password);
+
+let Student_age;
+
+console.table([account_Email, account_Id, Student_age])
